107f9c463c2289c8651b5802fdc567f15be3f18e
Morgan Wall <mkywall3@gmail.com>
2025-08-04 17:38:05 -0700

    dependencies

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pycrucible",
+    version="1.0.0",
+    author="mkywall",
+    author_email="mkywall3@gmail.com",
+    description="Python Client for the Crucible API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests>=2.25.0",
+        "pytz>=2021.1",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "black>=21.0",
+            "flake8>=3.8",
+            "mypy>=0.812",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "pycrucible=pycrucible:main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
