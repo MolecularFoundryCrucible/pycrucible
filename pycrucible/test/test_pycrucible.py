1c823ce8f087d6e6424bc618b9301c464eb22af3
Peter Ercius ncem-gauss jupyter <percius.lbl.gov>
2025-09-17 10:16:46 -0700

    add client to each test

---
+++ pycrucible/test/test_pycrucible.py
@@ -0,0 +1,81 @@
+"""
+Tests for the basic functionality of the smv io module.
+"""
+
+import pytest
+
+from pathlib import Path
+import tempfile
+
+from pycrucible import CrucibleClient, SecureInput
+
+API_URL = "https://crucible.lbl.gov/testapi"
+API_KEY = ""
+dataset0 = '0swk4bk8exsjh000nzvhp6hj28'
+
+
+class Test:
+    """
+    Test pycrucible
+    """
+    #self.dataset0 = '0swk4bk8exsjh000nzvhp6hj28' # ser file
+    #self.dataset1 = '0t5fbfwr31yq500095ms211vyr' # bcf, but does not exist
+    #self.dataset2 = '0swk4ddsqhy2v0004a57cb882c' # public
+
+    
+
+    @pytest.fixture
+    def temp_file(self):
+        """Create a temporary file and return the Path."""
+        tt = tempfile.NamedTemporaryFile(mode='wb')
+        tt.close()  # need to close the file to use it later
+        return Path(tt.name)
+
+    @pytest.fixture
+    def temp_dir(self, tmp_path):
+        """Create a temporary directory and return the Path."""
+        return tmp_path
+
+    def test_download_filename(self):
+        """specific filename"""
+        # Initialize the client
+        client = CrucibleClient(API_URL, API_KEY)
+        response = client.download_dataset(dataset0, 
+                                           file_name='09.28.46 Scanning Acquire_s-14o_34.7pm_12us_1.ser')
+        assert Path('./crucible-downloads/09.28.46 Scanning Acquire_s-14o_34.7pm_12us_1.ser').exists()
+        
+    def test_download_dataset_filename_dir(self, tmp_path):
+        """specific filename with directory path"""
+        client = CrucibleClient(API_URL, API_KEY)
+        response = client.download_dataset(dataset0, 
+                                           file_name='09.28.46 Scanning Acquire_s-14o_34.7pm_12us_1.ser', 
+                                           output_path=str(tmp_path)
+                                          )
+        assert (tmp_path / Path('09.28.46 Scanning Acquire_s-14o_34.7pm_12us_1.ser')).exists()
+        
+    def test_download_dataset_filename_file(self, tmp_path):
+        """specific filename with file path"""
+        client = CrucibleClient(API_URL, API_KEY)
+        output_path = tmp_path / Path('temp.ser')
+        response = client.download_dataset(dataset0, 
+                                           file_name='09.28.46 Scanning Acquire_s-14o_34.7pm_12us_1.ser', 
+                                           output_path=output_path
+                                          )
+        assert output_path.exists()
+    
+    def test_download_dataset_outputdir(self, tmp_path):
+        """specific filename with directory path"""
+        client = CrucibleClient(API_URL, API_KEY)
+        response = client.download_dataset(dataset0,
+                                           output_path=str(tmp_path)
+                                          )
+        assert (tmp_path / Path('09.28.46 Scanning Acquire_s-14o_34.7pm_12us_1.ser')).exists()
+        
+    def test_download_dataset_outputfile(self, tmp_path):
+        """specific filename with file path"""
+        client = CrucibleClient(API_URL, API_KEY)
+        output_path = tmp_path / Path('temp.ser')
+        response = client.download_dataset(dataset0,
+                                           output_path=output_path
+                                          )
+        assert output_path.exists()
